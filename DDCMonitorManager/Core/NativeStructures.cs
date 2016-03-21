fe6d10a1a8bdae3b3ffa70d948e1bab66d81a1de
Adam Jež <adamjez@outlook.cz>
2016-03-21 03:18:21 +0100

    Added systray support via NotifyIcon and added missing files

---
+++ DDCMonitorManager/Core/NativeStructures.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace DDCMonitorManager.Core
+{
+    public class NativeStructures
+    {
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Unicode)]
+        public struct PHYSICAL_MONITOR
+        {
+            public IntPtr hPhysicalMonitor;
+
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 128)]
+            public string szPhysicalMonitorDescription;
+        }
+
+        public enum MC_DISPLAY_TECHNOLOGY_TYPE
+        {
+            MC_SHADOW_MASK_CATHODE_RAY_TUBE,
+
+            MC_APERTURE_GRILL_CATHODE_RAY_TUBE,
+
+            MC_THIN_FILM_TRANSISTOR,
+
+            MC_LIQUID_CRYSTAL_ON_SILICON,
+
+            MC_PLASMA,
+
+            MC_ORGANIC_LIGHT_EMITTING_DIODE,
+
+            MC_ELECTROLUMINESCENT,
+
+            MC_MICROELECTROMECHANICAL,
+
+            MC_FIELD_EMISSION_DEVICE,
+        }
+    }
+}
---
+++ DDCMonitorManager/ViewModels/MainViewModel.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows;
+using System.Windows.Interop;
+using DDCMonitorManager.Annotations;
+using DDCMonitorManager.Core;
+
+namespace DDCMonitorManager.ViewModels
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        private readonly BrightnessControl brightnessControl;
+
+        public MainViewModel(Window window)
+        {
+            Sliders = new ObservableCollection<SliderViewModel>();
+
+            var wih = new WindowInteropHelper(window);
+            brightnessControl = new BrightnessControl(wih.Handle);
+            InitializeSliders();
+        }
+
+        public ObservableCollection<SliderViewModel> Sliders { get; private set; }
+
+        public void ChangeSliderValue(SliderViewModel slider, short value)
+        {
+            if (slider == null)
+            {
+                throw new ArgumentNullException(nameof(slider));
+            }
+            
+            brightnessControl.SetBrightness(value, slider.Id);
+            
+        }
+
+        private void InitializeSliders()
+        {
+            var monitorsCount = brightnessControl.GetMonitors();
+            for (var i = 0; i < monitorsCount; ++i)
+            {
+                var mInfo = brightnessControl.GetBrightnessCapabilities(i);
+                Sliders.Add(new SliderViewModel(mInfo, i));
+            }
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
